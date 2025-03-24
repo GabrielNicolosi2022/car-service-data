976832907e98c7e8a5b023cfca814ba197551a0b
Gabriel Nicolosi <gabianp@live.com.ar>
2025-03-24 19:52:55 -0300

    I create uploader with multer and user mocks files

---
+++ test/mocks/user.mock.js
@@ -0,0 +1,39 @@
+export const invalidFormat_1 = "Invalid format request";
+
+export const invalidFormat_2 = {};
+
+export const mockIncompleteUser = {
+  first_name: "Jose",
+  last_name: "Moñoz",
+  email: "josemunoz@gmail.com",
+  pass: "miPass1234",
+  thumbnail: "path/to/the/photo.png",
+};
+
+export const mockInvalidUser = {
+  first_name: "Jose",
+  last_name: "Moñoz",
+  username: 123456,
+  email: "josemunoz@gmail.com",
+  pass: "miPass1234",
+  thumbnail: "path/to/the/photo.png",
+};
+
+export const mockUserAdmin = {
+  first_name: "Gabriel",
+  last_name: "Nicolosi",
+  username: "Gabrieldev",
+  email: "gabrielDev80@gmail.com",
+  pass: "hashedPass80",
+  role: "admin",
+};
+
+export const mockCommonUser = {
+  first_name: "Miguel",
+  last_name: "Perez",
+  username: "miguerez",
+  phone: "1150485672",
+  email: "miguerez@gmail.com",
+  pass: "Password1234",
+  thumbnail: "path/to/the/photo.png",
+};
