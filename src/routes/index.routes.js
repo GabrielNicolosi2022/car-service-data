1a30c0ee4806b0bb615f99448e69ac9f664eb838
Gabriel Nicolosi <gabianp@live.com.ar>
2025-04-24 09:39:21 -0300

    vehicle.routes incorporated in index.routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import userRouter from "./user.routes.js";
 import sessionRouter from "./session.routes.js";
+import vehicleRouter from "./vehicle.routes.js";
+import { sessionInitVeryfication } from "../middlewares/common.middlewares.js";
 
 const indexRouter = Router();
 
 indexRouter.use("/v1/api/sessions", sessionRouter);
-indexRouter.use("/v1/api/users", userRouter);
+indexRouter.use("/v1/api/users", sessionInitVeryfication, userRouter);
+indexRouter.use("/v1/api/vehicles", sessionInitVeryfication, vehicleRouter);
 
 export default indexRouter;
