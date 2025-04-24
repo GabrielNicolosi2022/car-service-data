a4181cd8b65524ec7c02a3bce4f0b56f8d2ad8e2
Gabriel Nicolosi <gabianp@live.com.ar>
2025-04-24 08:48:51 -0300

    I create vehicles controllers

---
+++ src/controllers/vehicle.controller.js
@@ -0,0 +1,179 @@
+import * as service from "../services/vehicle.services.js";
+import getLogger from "../config/customLogger.js";
+
+const log = getLogger();
+
+const createVehicle = async (req, res) => {
+  const data = req.body;
+  try {
+    const vehicle = await service.create(data);
+    if (!vehicle) {
+      log.error("Error creating vehicle", { data });
+      return res
+        .status(400)
+        .json({ status: "Error", message: "Error creating vehicle" });
+    }
+    res.ststus(201).json({
+      stattus: "Success",
+      message: "Vehicle created successfully",
+      payload: vehicle,
+    });
+  } catch (error) {
+    log.fatal("createVehicle controller - Internal Server Error: ", error);
+    res.status(500).json({
+      status: "Error",
+      message: "Internal Server Error",
+    });
+  }
+};
+
+const getAllVehicles = async (req, res) => {
+  try {
+    const vehicles = await service.getAll();
+    if (!vehicles) {
+      log.error("getAllVehicles controller - No vehicles found");
+      return res.ststus(404).json({ status: "Error", message: "Not Found" });
+    }
+
+    res.status(200).json({
+      status: "Success",
+      message: "Vehicles listed successfully",
+      payload: vehicles,
+    });
+  } catch (error) {
+    log.fatal("getAllVehicles controller - Internal ServerError: ", error);
+    res.status(500).json({ status: "Error", message: "Internal server Error" });
+  }
+};
+
+const getVehicleById = async (req, res) => {
+  const id = req.params;
+  try {
+    const vehicle = await service.getById(id);
+    if (!vehicle) {
+      log.error("getVehicleById controller - Vehicle not found: ", { id });
+      return res
+        .status(404)
+        .json({ status: "Error", message: "Vehicle not Found" });
+    }
+    res.status(200).json({
+      status: "Success",
+      message: "Vehicle found successfully",
+      payload: vehicle,
+    });
+  } catch (error) {
+    log.fatal("getVehicleById controller - Vehicle not found: ", error);
+    res.status(500).json({
+      status: "Error",
+      message: "Internal Server Error",
+    });
+  }
+};
+
+const getVehicleByRegistration = async (res, res) => {
+  const registration = req.body;
+  try {
+    const vehicle = await service.getByRegistration(registration);
+    if (!vehicle) {
+      log.error("getVehicleByRegistration controller - Vehicle not found: ", {
+        registration,
+      });
+      return res.status(404).json({ status: "Error", message: "Not found" });
+    }
+
+    res.status(200).json({
+      status: "Success",
+      message: "Vehicle found succesfully",
+      payload: vehicle,
+    });
+  } catch (error) {
+    log.fatal(
+      "getVehicleByRegistration controller - Vehicle not found: ",
+      error
+    );
+    res.status(500).json({
+      status: "Error",
+      message: "Internal Server Error",
+    });
+  }
+};
+
+const updateVehicleById = async (req, res) => {
+  const id = req.params;
+  const data = req.body;
+  try {
+    const vehicle = await service.update(id, data);
+    if (!vehicle) {
+      log.error("updateVehicleById controller - Vehicle not found: ", { id });
+      return res
+        .status(404)
+        .json({ status: "Error", message: "Vehicle not found" });
+    }
+    res.status(200).json({
+      status: "Success",
+      message: "Vehicle updated successfully",
+      payload: vehicle,
+    });
+  } catch (error) {
+    log.fatal("updateVehicleById controller - Internal Server Error: ", error);
+    res.status(500).json({
+      status: "Error",
+      message: "Internal Servefr Error",
+    });
+  }
+};
+
+const updateVehicleByRegistration = async (req, res) => {
+  const registration = req.params;
+  const data = req.body;
+  try {
+    const vehicle = await service.update(registration, data);
+    if (!vehicle) {
+      log.error("updateVehicleById controller - Vehicle not found: ", {
+        registration,
+      });
+      return res
+        .status(404)
+        .json({ status: "Error", message: "Vehicle not found" });
+    }
+    res.status(200).json({
+      status: "Success",
+      message: "Vehicle updated successfully",
+      payload: vehicle,
+    });
+  } catch (error) {
+    log.fatal("updateVehicleById controller - Internal Server Error: ", error);
+    res.status(500).json({
+      status: "Error",
+      message: "Internal Servefr Error",
+    });
+  }
+};
+
+const deleteVehicle = async (req, res) => {
+  const id = req.params;
+  try {
+    const deletedVehicle = await service.eliminate(id);
+    if (!deletedVehicle) {
+      log.error("deleteVehicle controller - Vehicle not found: ", { id });
+      return res.status(404).json({ status: "Error", message: "Not found" });
+    }
+    res.status(200).json({
+      status: "Success",
+      message: "Vehicle deleted successfully",
+    });
+  } catch (error) {
+    log.fatal("deleteVehicle controller - Internal Server Error: ", error);
+    res.status(500).json({ status: "Error", message: "Internal Server Error" });
+  }
+};
+
+export {
+  createVehicle,
+  getAllVehicles,
+  getVehicleById,
+  getVehicleByRegistration,
+  updateVehicleById,
+  updateVehicleByRegistration,
+  deleteVehicle,
+};
