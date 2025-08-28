be18a99040b4b693bd1e5a59c67eb0e6941e420b
Gabriel Nicolosi <gabianp@live.com.ar>
2025-08-28 20:16:09 -0300

    I updates sessionInitVeryfication middleware and create verificateOwner middleware.

--- src/middlewares/common.middlewares.js
+++ src/middlewares/common.middlewares.js
@@ -3,9 +3,10 @@ import getLogger from "../utils/logger.utils.js";
 const log = getLogger();
 
 export const sessionInitVeryfication = (req, res, next) => {
-  if (!req.session.user) {
+  if (!req.session) {
     log.error("sessionInitVerification - Unauthorized access to this resource");
     return res.status(401).json({ status: "Error", message: "Unauthorized" });
   }
+  // console.log("sessionInitVeryfication - req.session: ", req.session);
   next();
 };
--- src/middlewares/vehicles.middlewares.js
+++ src/middlewares/vehicles.middlewares.js
@@ -1,3 +1,7 @@
 import getLogger from "../utils/logger.utils";
 
 const log = getLogger();
+
+const verificateOwner = (req, res, next) => {
+  const { owner } = req.session.user;
+};
